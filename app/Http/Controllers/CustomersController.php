b33907a29d21adb052827036a76ca8332d090bad
mather <misaka@misaka.im>
2019-08-26 18:03:05 +0800

    eloquent where clause

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -9,22 +9,23 @@ class CustomersController extends Controller
 {
     public function list()
     {
-        $customers = Customer::all();
+    	  $activeCustomers = Customer::where('active', 1)->get();
+	      $inactiveCustomers = Customer::where('active', 0)->get();
 
-        return view('internals.customers', [
-            'customers' => $customers
-        ]);
+        return view('internals.customers', compact('activeCustomers', 'inactiveCustomers'));
     }
 
     public function store()
     {
         $data = request()->validate([
             'name' => 'required|min:3',
-            'email' => 'required|email'
+            'email' => 'required|email',
+		        'active' => 'required'
         ]);
         $customers = new Customer();
         $customers->name = request('name');
         $customers->email = request('email');
+	      $customers->active = request('active');
         $customers->save();
         return back();
     }
--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -15,14 +15,24 @@
 
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" name="name" value="{{ old('name') }}">
+                    <input type="text" name="name" value="{{ old('name') }}" class="form-control">
                     <div>{{ $errors->first('name') }}</div>
                 </div>
 
                 <div class="form-group">
                     <label for="name">Email</label>
-                    <input type="text" name="email" value="{{ old('email') }}">
+                    <input type="text" name="email" value="{{ old('email') }}" class="form-control">
                     <div>{{ $errors->first('email') }}</div>
+                </div>
+
+                <div class="form-group">
+                    <label for="active">Status</label>
+                    <select name="active" id="active" class="form-control">
+                        <option value="" disabled>Select customer status</option>
+                        <option value="1">Active</option>
+                        <option value="0">Inactive</option>
+                    </select>
+
                 </div>
 
                 <button type="submit" class="btn btn-primary">Add Customer</button>
@@ -33,9 +43,19 @@
     </div>
 
     <div class="row">
-        <div class="col-12">
+        <div class="col-6">
+            <h3>Active Customers</h3>
             <ul>
-                @foreach ($customers as $item)
+                @foreach ($activeCustomers as $item)
+                    <li> {{ $item->name }} <span class="">({{ $item->email }})</span></li>
+                @endforeach
+            </ul>
+        </div>
+
+        <div class="col-6">
+            <h3>Inactive Customers</h3>
+            <ul>
+                @foreach ($inactiveCustomers as $item)
                     <li> {{ $item->name }} <span class="">({{ $item->email }})</span></li>
                 @endforeach
             </ul>
