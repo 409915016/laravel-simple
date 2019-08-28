85c566d74208d6f7530f115a13bde19f00e9dd17
mather <misaka@misaka.im>
2019-08-28 15:24:26 +0800

    Artisan Authentication - Register, Login & Password Reset

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,6 @@ Route::view('about', 'about');
 //Route::delete('customers/{customer}', 'CustomersController@destroy');
 
 Route::resource('customers', 'CustomersController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
