e84cc5aa4d62abb169cd1f859487b42888a2f36f
mather <misaka@misaka.im>
2019-08-31 18:33:22 +0800

    Artisan Commands (Closure)

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use \App\Company;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,14 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('contact:company-clean', function(){
+    $this->info('Cleaning');
+
+    Company::whereDoesntHave('customers')
+        ->get()
+        ->each(function ($company){
+            $company->delete();
+            $this->warn('Delected: '. $company->name);
+        });
+})->describe('Cleans Up Unused Companies');
