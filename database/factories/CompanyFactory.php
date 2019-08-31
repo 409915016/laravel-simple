cdb07b78a072fd43f6e18ae3fc68830bddc8b289
mather <misaka@misaka.im>
2019-08-31 21:01:06 +0800

    Model Factories

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Company;
+use Faker\Generator as Faker;
+
+$factory->define(Company::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'phone' => $faker->phoneNumber
+
+    ];
+});
